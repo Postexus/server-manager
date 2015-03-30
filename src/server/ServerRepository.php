00f34bdc9b11f9b6ba808610f8ed1a465a4e33f0
Maarten Kossen <hello@mpkossen.com>
2015-03-30 16:07:22 +0200

    Add saveServer method.

--- src/server/ServerRepository.php
+++ src/server/ServerRepository.php
@@ -5,11 +5,31 @@ namespace serverManager\src\server;
 
 class ServerRepository
 {
+    /** @var \PDO */
+    private $DB;
+
+    /**
+     * @param \PDO $DB
+     */
+    public function __construct(\PDO $DB)
+    {
+        $this->DB = $DB;
+    }
+
     /**
      * @return Server
      */
     public function createServer()
     {
         return new Server();
+    }
+
+    /**
+     * @param Server $Server
+     * @return bool
+     */
+    public function saveServer(Server $Server)
+    {
+        return true;
     }
 }
--- test/ServerRepositoryTest.php
+++ test/ServerRepositoryTest.php
@@ -3,14 +3,32 @@
 namespace serverManager\test;
 
 
+use serverManager\src\server\ServerApi;
 use serverManager\src\server\ServerRepository;
 
 class ServerRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     public function test_createServer_returnsServerObject()
     {
-        $ServerRepository = new ServerRepository();
+        $DB = $this->getMockBuilder('\PDO')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $ServerRepository = new ServerRepository($DB);
 
         $this->assertInstanceOf('serverManager\\src\\server\\Server', $ServerRepository->createServer());
+    }
+
+    public function test_saveServer_returnsTrue()
+    {
+        $DB = $this->getMockBuilder('PDO')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $ServerRepository = new ServerRepository($DB);
+        $ServerApi = new ServerApi($ServerRepository);
+        $Server = $ServerApi->createServer();
+
+        $this->assertEquals(true, $ServerRepository->saveServer($Server));
     }
 }
